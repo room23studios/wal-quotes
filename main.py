3a7ac95d2681d6a13a337cd7a60bc9a36a0ed3a1
Jakub Jendryka <jendryka.j@gmail.com>
2018-03-01 18:41:36 +0100

    Changed encoding and renamed api/id/ to api/quote/

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask import jsonify, request, abort, g
+from flask import request, abort, g
 from passlib.apps import custom_app_context as pwd_context
 from flask_httpauth import HTTPBasicAuth
 from sqlalchemy.sql.expression import func, desc
+import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -36,37 +37,45 @@ class Daily(db.Model):
     quote_id = db.Column(db.Integer)
 
 
-@app.route('/api/id/<int:quote_id>')
+@app.route('/api/quote/<int:quote_id>')
 def get_quote(quote_id):
-    quote_db = Quote.query.filter_by(id=quote_id).first()
+    quote_db = Quote.query.filter_by(id=quote_id).filter_by(accepted=True).first()
     if quote_db is not None:
-        if quote_db.accepted:
-            quote = {'quote': quote_db.quote,
-                     'date': quote_db.date,
-                     'annotation': quote_db.annotation}
-            return jsonify({'status': 'success',
-                            'quote': quote})
-        else:
-            return jsonify({'status': 'error',
-                            'description': 'Quote does not exists'})
+        quote = {'quote': quote_db.quote,
+                 'date': quote_db.date,
+                 'annotation': quote_db.annotation}
+        return json.dumps({'status': 'success',
+                           'quote': quote}, ensure_ascii=False).encode('utf8')
     else:
-        return jsonify({'status': 'error',
-                        'description': 'Quote does not exists'})
+        return json.dumps({'status': 'error',
+                           'description': 'Quote does not exist'}, ensure_ascii=False).encode('utf8')
+
+
+@app.route('/api/quotes')
+def get_quotes():
+    quotes = Quote.query.filter_by(accepted=True)
+    output = []
+    for quote in quotes:
+        output.append({'quote': quote.quote,
+                       'date': quote.date,
+                       'annotation': quote.annotation})
+    return json.dumps({'status': 'success',
+                       'quotes': output}, ensure_ascii=False).encode('utf8')
 
 
 @app.route('/api/submit', methods=['POST'])
 def submit():
-    if Quote.query.filter_by(quote=request.form['Quote']).scalar() is None:
+    if Quote.query.filter_by(quote=request.form['Quote']).first() is None:
         quote = Quote(quote=request.form['Quote'], date=request.form.get('Date', ''),
                       annotation=request.form.get('Annotation', ''))
         db.session.add(quote)
         db.session.commit()
-        return jsonify({'status': 'success',
-                        'id': quote.id})
+        return json.dumps({'status': 'success',
+                           'id': quote.id}, ensure_ascii=False).encode('utf8')
 
     else:
-        return jsonify({'status': 'error',
-                        'description': 'Quote is not unique'})
+        return json.dumps({'status': 'error',
+                           'description': 'Quote is not unique'}, ensure_ascii=False).encode('utf8')
 
 
 @auth.verify_password
@@ -88,8 +97,8 @@ def get_submissions():
                        'quote': quote.quote,
                        'date': quote.date,
                        'annotation': quote.annotation})
-    return jsonify({'status': 'success',
-                    'submissions': output})
+    return json.dumps({'status': 'success',
+                       'submissions': output}, ensure_ascii=False).encode('utf8')
 
 
 @app.route('/api/delete/<int:quote_id>', methods=['POST'])
@@ -99,10 +108,10 @@ def remove_quote(quote_id):
     if quote_db is not None:
         Quote.query.filter_by(id=quote_id).delete()
         db.session.commit()
-        return jsonify({'status': 'success'})
+        return json.dumps({'status': 'success'}, ensure_ascii=False).encode('utf8')
     else:
-        return jsonify({'status': 'error',
-                        'description': 'Quote does not exists'})
+        return json.dumps({'status': 'error',
+                           'description': 'Quote does not exist'}, ensure_ascii=False).encode('utf8')
 
 
 @app.route('/api/submissions/<int:quote_id>', methods=['POST'])
@@ -113,24 +122,24 @@ def set_submissions(quote_id):
         quote_db.accepted = True
         db.session.add(quote_db)
         db.session.commit()
-        return jsonify({'status': 'success'})
+        return json.dumps({'status': 'success'}, ensure_ascii=False).encode('utf8')
     else:
-        return jsonify({'status': 'error',
-                        'description': 'Quote does not exists'})
+        return json.dumps({'status': 'error',
+                           'description': 'Quote does not exist'}, ensure_ascii=False).encode('utf8')
 
 
 @app.route('/api/new_admin', methods=['POST'])
 @auth.login_required
 def add_admin():
-    if Admin.query.filter_by(username=request.form['Username']).scalar() is None:
+    if Admin.query.filter_by(username=request.form['Username']).first() is None:
         admin = Admin(username=request.form['Username'])
         admin.hash_password(request.form['Username'])
         db.session.add(admin)
         db.session.commit()
-        return jsonify({'status': 'success'})
+        return json.dumps({'status': 'success'}, ensure_ascii=False).encode('utf8')
     else:
-        return jsonify({'status': 'error',
-                        'description': 'Username is not unique'})
+        return json.dumps({'status': 'error',
+                           'description': 'Username is not unique'}, ensure_ascii=False).encode('utf8')
 
 
 @app.route('/api/change_password', methods=['POST'])
@@ -139,7 +148,7 @@ def change_password():
     g.user.hash_password(request.form['Password'])
     db.session.add(g.user)
     db.session.commit()
-    return jsonify({'status': 'success'})
+    return json.dumps({'status': 'success'}, ensure_ascii=False).encode('utf8')
 
 
 @app.route('/api/random')
@@ -150,11 +159,11 @@ def get_random_quote():
                  'quote': quote_db.quote,
                  'date': quote_db.date,
                  'annotation': quote_db.annotation}
-        return jsonify({'status': 'success',
-                        'quote': quote})
+        return json.dumps({'status': 'success',
+                           'quote': quote}, ensure_ascii=False).encode('utf8')
     else:
-        return jsonify({'status': 'error',
-                        'description': 'No accepted quotes in database'})
+        return json.dumps({'status': 'error',
+                           'description': 'No accepted quotes in database'}, ensure_ascii=False).encode('utf8')
 
 
 @app.route('/api/daily')
@@ -166,21 +175,21 @@ def get_daily():
                  'quote': quote_db.quote,
                  'date': quote_db.date,
                  'annotation': quote_db.annotation}
-        return jsonify({'status': 'success',
-                        'quote': quote})
+        return json.dumps({'status': 'success',
+                           'quote': quote}, ensure_ascii=False).encode('utf8')
     else:
-        return jsonify({'status': 'error',
-                        'description': 'No daily quote'})
+        return json.dumps({'status': 'error',
+                           'description': 'No daily quote'}, ensure_ascii=False).encode('utf8')
 
 
 @app.route('/api/new_daily', methods=['POST'])
 @auth.login_required
 def new_daily():
     if not change_daily():
-        return jsonify({'status': 'error',
-                        'description': 'No accepted quotes in database'})
-    return jsonify({'status': 'success',
-                    'id': Daily.query.order_by(desc(Daily.id)).first().quote_id})
+        return json.dumps({'status': 'error',
+                           'description': 'No accepted quotes in database'}, ensure_ascii=False).encode('utf8')
+    return json.dumps({'status': 'success',
+                       'id': Daily.query.order_by(desc(Daily.id)).first().quote_id}, ensure_ascii=False).encode('utf8')
 
 
 def change_daily():
